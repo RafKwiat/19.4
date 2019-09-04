0287888ea7fecf9165467e5b8869aff79325ae20
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-09-04 22:56:09 +0200

    Final data

--- script-compiled.js
+++ script-compiled.js
@@ -6,7 +6,7 @@ var x = 'Hello';
 var y = 'World';
 var sayHello = x + ' ' + y;
 
-console.log('Połączone wyrażenie: ' + sayHello);
+console.log('Po\u0142\u0105czone wyra\u017Cenie: ' + sayHello);
 
 //Zad. 2
 
@@ -14,26 +14,26 @@ var multiply = function multiply(x) {
   var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
   return x * y;
 };
-console.log('Wynik mnożenia: ' + multiply(5, 6));
-console.log('Wynik mnożenia: ' + multiply(6, 6));
+console.log('Wynik mno\u017Cenia: ' + multiply(5, 6));
+console.log('Wynik mno\u017Cenia: ' + multiply(6, 6));
 
 //Zad. 3
 
-var avarage = function avarage() {
+var average = function average() {
   for (var _len = arguments.length, numbers = Array(_len), _key = 0; _key < _len; _key++) {
     numbers[_key] = arguments[_key];
   }
 
   return numbers.reduce(function (a, b) {
-    return (a + b) / numbers.length;
-  });
+    return a + b;
+  }, 0) / numbers.length;
 };
-console.log('Średnia arytmetyczna: ' + avarage(1, 3, 5, 4, 8));
+console.log('\u015Arednia: ' + average(1, 3, 6, 6, 52));
 
 //Zad. 4
 
 var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-console.log('Średnia ocen: ' + avarage.apply(undefined, grades));
+console.log('\u015Arednia ocen: ' + average.apply(undefined, grades));
 
 //Zad. 5
 
@@ -41,4 +41,4 @@ var data = [1, 4, 'Iwona', false, 'Nowak'];
 var firstName = data[2],
     lastName = data[4];
 
-console.log('Imię: ' + firstName + 'Nazwisko: ' + lastName);
+console.log('Imi\u0119:  ' + firstName + '  Nazwisko: ' + lastName);
--- script.js
+++ script.js
@@ -4,26 +4,27 @@ const x = 'Hello';
 const y = 'World';
 const sayHello = `${x} ${y}`
 
-console.log('Połączone wyrażenie: ' + sayHello);
+console.log(`Połączone wyrażenie: ${sayHello}`);
 
 //Zad. 2
 
 let multiply=(x, y=1) => {return x * y};
-console.log('Wynik mnożenia: ' + multiply(5, 6));
-console.log('Wynik mnożenia: ' + multiply(6, 6));
+console.log(`Wynik mnożenia: ${multiply(5, 6)}`);
+console.log(`Wynik mnożenia: ${multiply(6, 6)}`);
 
 //Zad. 3
 
-let avarage = (...numbers) => numbers.reduce((a, b) => (a + b) / numbers.length);
-console.log('Średnia arytmetyczna: ' + avarage(1,3,5,4,8));
+let average = (...numbers) => numbers.reduce((a, b) => a + b, 0) / numbers.length;
+console.log(`Średnia: ${average(1,3,6,6,52)}`);
 
 //Zad. 4
 
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-console.log('Średnia ocen: ' + avarage(...grades));
+console.log(`Średnia ocen: ${average(...grades)}`);
 
 //Zad. 5
 
 const data = [1, 4, 'Iwona', false, 'Nowak'];
-const [, ,firstName, ,lastName] = data;
-console.log('Imię: ' + firstName + 'Nazwisko: ' + lastName);
+const [, , firstName, ,lastName] = data;
+console.log(`Imię:  ${firstName}  Nazwisko: ${lastName}`);
+
